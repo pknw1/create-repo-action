7c470b53902078b234ba4341a25ed6ae2115dc29
PK <pknw1@users.noreply.github.com>
2023-04-10 01:52:52 +0100

    Update index.js
    temp

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
+const sourceRepoName = core.getInput('source-repo');
 const targetRepoName = core.getInput('repo-name');
 const ghToken = core.getInput('org-admin-token');
 var createRepoData = JSON.stringify(
   {
-    "name":targetRepoName,
-    "private":true,
-    "visibility":"private"
+    "owner":"NEW-REPO-OWNER",
+    "name":"NEW-REPO-NAME",
+    "description":"Repo created from a template",
+    "include_all_branches":false,
+    "private":true}'
   }
 );
 const targetOrgName = github.context.payload.repository.owner.login;
 
 var config = {
   method: 'post',
-  url: 'https://api.github.com/orgs/'+targetOrgName+'/repos',
+  url: 'https://api.github.com/repos/'+targetOrgName+'/'+sourceRepoName+'/generate',
   headers: { 
     'Accept': 'application/vnd.github.v3+json', 
-    'Authorization': 'token '+ghToken, 
+    'Authorization': 'Bearer '+ghToken, 
     'Content-Type': 'application/json'
   },
   data : createRepoData
