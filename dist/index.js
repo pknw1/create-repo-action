c508fe972f8dcb998b67334487dc6a35425cc0ac
PK <pknw1@users.noreply.github.com>
2023-04-10 03:18:48 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -11496,7 +11496,7 @@ const targetOrgName = github.context.payload.repository.owner.login;
 
 var config = {
   method: 'post',
-  url: 'https://api.github.com/repos/'+targetOrgName+'/'+targetOrgName+'/generate',
+  url: 'https://api.github.com/repos/'+targetOrgName+'/'+sourceRepoName+'/generate',
   headers: { 
     'Accept': 'application/vnd.github.v3+json', 
     'Authorization': 'token '+ghToken, 
